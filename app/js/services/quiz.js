fd3501a10e66dc3731648cbc39dfe106602d4e55
Daniel Leinbach <leinb1dr@gmail.com>
2014-03-23 17:41:51 -0400

    updating quiz js

--- app/js/services/quiz.js
+++ app/js/services/quiz.js
@@ -1,9 +1,9 @@
 angular.module('app.service.Quiz', [])
 
-.service('QuizService', ['$rootScope','$http', '$timeout', function ($rootScope, $http, $timeout) {
+.service('QuizService', ['$rootScope','$http', '$timeout',function ($rootScope, $http, $timeout,$scope) {
 
 	this.quiz;
-	this.currentQuestion;
+	this.currentQuestion = {"question":"question2","choices":["answer1","answer2","answer3","answer4"], "answer":1};
 	this.currentIndex = -1;
 	this.answers = {};
 
@@ -12,7 +12,9 @@ angular.module('app.service.Quiz', [])
 		if(_this.currentIndex < _this.quiz.questions.length){
 			_this.currentQuestion = _this.quiz.questions[_this.currentIndex];
 			_this.answers = {};
-			$timeout(_this.timeUp, 30000);
+			$timeout(function(event){
+				_this.nextQuestion(_this)
+			}, 30000);
 		}
 	}
 
@@ -21,6 +23,7 @@ angular.module('app.service.Quiz', [])
 		$http.get("quizes/quiz1.json").success(function(data, status, headers, config){
 			_this.quiz = data;
 			_this.nextQuestion(_this);
+			$rootScope.$apply();
 		});
 	}
 
@@ -29,10 +32,6 @@ angular.module('app.service.Quiz', [])
 		var senderId = event.senderId;
 		answers[senderId] = message;
 
-	}
-
-	this.timeUp = function(event){
-		this.nextQuestion()
 	}
 
 
