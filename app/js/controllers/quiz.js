845137e8b00836a2e608bd6f0ac5b3bdd9cebccc
Josh-Nimtz <josh.nimtz@compuware.com>
2014-03-23 18:28:33 -0400

    Started lobby view.

--- app/js/controllers/quiz.js
+++ app/js/controllers/quiz.js
@@ -1,6 +1,29 @@
 angular.module('app.controller.Quiz', [])
 
 .controller('QuizCtrl', ['$scope', 'CastService','QuizService', function ($scope, CastService, QuizService) {
+
 	$scope.messages = CastService.messages;
-	$scope.currentQuestion = QuizService.currentQuestion
+
+	$scope.currentQuestion = QuizService.currentQuestion;
+
+	$scope.player1 = {
+		senderId: 1,
+		name: "Player 1"
+	};
+
+	$scope.player2 = {
+		senderId: 2,
+		name: "Player 2"
+	};
+
+	$scope.player3 = {
+		senderId: 3,
+		name: "Player 3"
+	};
+
+	$scope.player4 = {
+		senderId: 4,
+		name: "Player 4"
+	};
+
 }]);
